7a3091cf47a464bfe895042912e63ecde821b4bb
Thim Cederlund <thim@cederlund.de>
2023-09-24 19:04:34 +0200

    refactor: fix indentation

--- config.h
+++ config.h
@@ -1,20 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
-/* how many entries scm will keep track of */
-#define MAXENTRIES 10
+/* how many clipboard entries scm will keep track of */
+#define MAXENTRIES	10
 
-/* file mask for all clip entries, see chmod(1) */
-#define FILEMASK S_IRUSR | S_IWUSR
+/* file mask for all entries, see chmod(1) */
+#define FILEMASK	S_IRUSR | S_IWUSR
 
-/* if any clipboard activity comes from a title matched below it'll be ignored */
-static const char *ignore_rules[] = {
-	".*— Mozilla Firefox$",
-};
+/* the working directory that contains the clipboard entries, set via -d flag */
+static char *maindir = NULL;
 
-/* directory containing all clipboard entries, set via -d flag */
-static char *maindir;
-
-#define FLAG_PRIMARY (1 << 0)
-#define FLAG_VERBOSE (1 << 1)
-#define FLAG_ONESHOT (1 << 2)
+#define FLAG_PRIMARY	(1 << 0)
+#define FLAG_VERBOSE	(1 << 1)
+#define FLAG_ONESHOT	(1 << 2)
+#define FLAG_KEEP		(1 << 3)
 extern int flags;
+
+// vim:ts=4 tw=4 cc=80
